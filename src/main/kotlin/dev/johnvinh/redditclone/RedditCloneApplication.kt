3f5d9a51294cf41462c4f10f3335cee2c1378711
John Vinh <john.michael.vinh2@gmail.com>
2023-03-25 21:26:55 -0400

    Initial commit

---
+++ src/main/kotlin/dev/johnvinh/redditclone/RedditCloneApplication.kt
@@ -0,0 +1,11 @@
+package dev.johnvinh.redditclone
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RedditCloneApplication
+
+fun main(args: Array<String>) {
+    runApplication<RedditCloneApplication>(*args)
+}
---
+++ src/test/kotlin/dev/johnvinh/redditclone/RedditCloneApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.johnvinh.redditclone
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RedditCloneApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
