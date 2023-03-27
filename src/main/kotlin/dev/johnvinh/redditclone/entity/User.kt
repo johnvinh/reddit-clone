262475b0938729cf56d894cb6611eccfd194111f
John Vinh <john.michael.vinh2@gmail.com>
2023-03-26 20:34:51 -0400

    Change table name

--- src/main/kotlin/dev/johnvinh/redditclone/entity/User.kt
+++ src/main/kotlin/dev/johnvinh/redditclone/entity/User.kt
@@ -3,8 +3,10 @@ package dev.johnvinh.redditclone.entity
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.Id
+import jakarta.persistence.Table
 
 @Entity
+@Table(name = "Users")
 class User(
     @Id @GeneratedValue var id: Long? = null,
 )
