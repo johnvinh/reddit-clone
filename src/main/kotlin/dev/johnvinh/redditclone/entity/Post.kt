a7d53559e9b4f1d3fbd30881b732e9942f3d39e7
John Vinh <john.michael.vinh2@gmail.com>
2023-03-26 20:22:57 -0400

    Add Post attributes

--- src/main/kotlin/dev/johnvinh/redditclone/entity/Post.kt
+++ src/main/kotlin/dev/johnvinh/redditclone/entity/Post.kt
@@ -1,4 +1,15 @@
 package dev.johnvinh.redditclone.entity
 
-class Post {
-}
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.Id
+import jakarta.persistence.OneToMany
+
+@Entity
+class Post(
+    @Id @GeneratedValue var id: Long? = null,
+    var title: String,
+    var textualContent: String?,
+    var link: String?,
+    @OneToMany var comments: List<Comment>
+)
