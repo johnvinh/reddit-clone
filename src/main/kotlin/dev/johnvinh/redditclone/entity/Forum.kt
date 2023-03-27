031f4333df3b176df58afdde6fcf28a772345dcb
John Vinh <john.michael.vinh2@gmail.com>
2023-03-26 20:45:04 -0400

    Add Forum attributes

--- src/main/kotlin/dev/johnvinh/redditclone/entity/Forum.kt
+++ src/main/kotlin/dev/johnvinh/redditclone/entity/Forum.kt
@@ -1,4 +1,17 @@
 package dev.johnvinh.redditclone.entity
 
-class Forum {
-}
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.Id
+import jakarta.persistence.OneToMany
+import jakarta.persistence.OneToOne
+
+@Entity
+class Forum(
+    @Id @GeneratedValue var id: Long? = null,
+    var name: String,
+    var description: String,
+    @OneToOne var owner: User,
+    @OneToMany var posts: List<Post>,
+    @OneToMany var moderators: List<User>,
+)
