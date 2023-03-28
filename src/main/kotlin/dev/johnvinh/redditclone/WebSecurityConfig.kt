b7a5231fdcd6239276cdc65633eaccb21b57ffce
John Vinh <john.michael.vinh2@gmail.com>
2023-03-28 10:51:14 -0400

    Initialize frontend

--- src/main/kotlin/dev/johnvinh/redditclone/WebSecurityConfig.kt
+++ src/main/kotlin/dev/johnvinh/redditclone/WebSecurityConfig.kt
@@ -1,4 +1,11 @@
 package dev.johnvinh.redditclone
 
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+
+@Configuration
+@EnableWebSecurity
 class WebSecurityConfig {
+
+    
 }
