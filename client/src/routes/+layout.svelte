28368bc1936370514f352612d21dc522d9c7b875
John Vinh <john.michael.vinh2@gmail.com>
2023-03-28 11:13:06 -0400

    Add Sakura stylesheet

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -3,8 +3,15 @@
 </script>
 
 <svelte:head>
-    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/sakura-earthly.css" type="text/css">
 </svelte:head>
+
+<header>
+    <h1>Reddit Clone</h1>
+    <nav>
+        
+    </nav>
+</header>
 <main>
     <slot></slot>
 </main>
