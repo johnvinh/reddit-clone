d92970c68de864ebf3108d7ee2127e5652668876
John Vinh <john.michael.vinh2@gmail.com>
2023-03-28 19:06:51 -0400

    Create GET endpoint for user api

--- src/main/kotlin/dev/johnvinh/redditclone/controller/UserController.kt
+++ src/main/kotlin/dev/johnvinh/redditclone/controller/UserController.kt
@@ -1,9 +1,17 @@
 package dev.johnvinh.redditclone.controller
 
+import dev.johnvinh.redditclone.entity.User
 import dev.johnvinh.redditclone.repository.UserRepository
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/user")
-class UserController(private val repository: UserRepository)
+class UserController(private val repository: UserRepository) {
+
+    @GetMapping("")
+    fun get(): Iterable<User> {
+        return repository.findAll()
+    }
+}
