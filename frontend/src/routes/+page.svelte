4a586802b85cbfc11fd1fde63b1679d445292052
John Vinh <john.michael.vinh2@gmail.com>
2023-03-28 19:07:16 -0400

    Initialize frontend with Svelte and set proxy

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    import {onMount} from "svelte";
+    let users = [];
+
+    onMount(async () => {
+        const res = await fetch("/api/user/");
+        users = res.json();
+    });
+    console.log(users);
+</script>
+
+<p>Hello!</p>
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,5 +2,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+	server: {
+		proxy: {
+			'/api': 'http://localhost:8080',
+		},
+	},
 	plugins: [sveltekit()]
 });
