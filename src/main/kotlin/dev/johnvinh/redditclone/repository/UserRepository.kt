a5d3c3cba0e33b0457c9074730cf13d7b8dec900
John Vinh <john.michael.vinh2@gmail.com>
2023-04-03 11:41:40 -0400

    Create find by username method in user repository

--- src/main/kotlin/dev/johnvinh/redditclone/repository/UserRepository.kt
+++ src/main/kotlin/dev/johnvinh/redditclone/repository/UserRepository.kt
@@ -3,4 +3,6 @@ package dev.johnvinh.redditclone.repository
 import dev.johnvinh.redditclone.entity.User
 import org.springframework.data.repository.CrudRepository
 
-interface UserRepository : CrudRepository<User, Long>
+interface UserRepository : CrudRepository<User, Long> {
+    fun findByUsername(username: String): User?
+}
