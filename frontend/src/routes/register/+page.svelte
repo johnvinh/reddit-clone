bf00ed99cc85630a7af5e959456b36c0b9f0fd56
John Vinh <john.michael.vinh2@gmail.com>
2023-04-03 12:44:05 -0400

    Show response of registration

--- frontend/src/routes/register/+page.svelte
+++ frontend/src/routes/register/+page.svelte
@@ -1,9 +1,43 @@
-<form method="post" action="/api/user/register">
+<script>
+
+    let username = '';
+    let password = '';
+    let message = '';
+
+    async function handleSubmit(e)
+    {
+        e.preventDefault();
+
+        const response = await fetch('/api/user/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                username,
+                password
+            })
+        });
+
+        const data = await response.json();
+
+        if (response.ok) {
+            message = "Registration successful!";
+        } else {
+            message = data.message;
+        }
+    }
+</script>
+
+{#if message}
+    <p>{message}</p>
+{/if}
+<form method="post" on:submit={handleSubmit}>
     <label for="username">Username</label>
-    <input id="username" name="username" type="text">
+    <input id="username" name="username" type="text" bind:value={username} required>
 
     <label for="password">Password</label>
-    <input id="password" name="password" type="password">
+    <input id="password" name="password" type="password" bind:value={password} required>
 
     <input name="submit" type="submit">
 </form>
