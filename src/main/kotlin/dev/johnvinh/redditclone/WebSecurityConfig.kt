fc06e2ce03ea442841330cd41606d25233aafecf
John Vinh <john.michael.vinh2@gmail.com>
2023-04-03 12:20:34 -0400

    Disable CSRF

--- src/main/kotlin/dev/johnvinh/redditclone/WebSecurityConfig.kt
+++ src/main/kotlin/dev/johnvinh/redditclone/WebSecurityConfig.kt
@@ -1,11 +1,19 @@
 package dev.johnvinh.redditclone
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.util.matcher.RequestMatcher
 
 @Configuration
 @EnableWebSecurity
 class WebSecurityConfig {
 
-    
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http.csrf().disable()
+        return http.build()
+    }
 }
