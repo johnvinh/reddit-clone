dc9fd76253de39d9b719e988bf4ed49847ab7f7c
John Vinh <john.michael.vinh2@gmail.com>
2023-04-04 14:23:07 -0400

    Switch PostController to use a service instead of repository

--- src/main/kotlin/dev/johnvinh/redditclone/controller/PostController.kt
+++ src/main/kotlin/dev/johnvinh/redditclone/controller/PostController.kt
@@ -1,9 +1,12 @@
 package dev.johnvinh.redditclone.controller
 
 import dev.johnvinh.redditclone.repository.PostRepository
+import dev.johnvinh.redditclone.service.PostService
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/post")
-class PostController(private val repository: PostRepository)
+class PostController(private val service: PostService) {
+
+}
--- src/main/kotlin/dev/johnvinh/redditclone/service/PostService.kt
+++ src/main/kotlin/dev/johnvinh/redditclone/service/PostService.kt
@@ -1,4 +1,13 @@
 package dev.johnvinh.redditclone.service
 
-class PostService {
+import dev.johnvinh.redditclone.entity.Post
+import dev.johnvinh.redditclone.repository.PostRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+
+@Service
+class PostService @Autowired constructor(private val repository: PostRepository) {
+    fun createPost(post: Post) {
+        repository.save(post)
+    }
 }
