d3443ece3cc1089c4f56db4ef87fab2621d3544d
John Vinh <john.michael.vinh2@gmail.com>
2023-04-11 20:26:52 -0400

    Implement displaying of posts on a single forum

--- frontend/src/routes/forums/[forum]/+page.svelte
+++ frontend/src/routes/forums/[forum]/+page.svelte
@@ -6,13 +6,17 @@
 
     let forumData = {};
     let message = "";
+    let posts = [];
 
     onMount(async () => {
         const response = await fetch(`/api/forum/${data.forum.name}`)
         const text = await response.text();
         if (text === "") {
             message = "There is no forum with that name.";
+            return;
         }
+        forumData = await response.json();
+        posts = forumData.posts;
     });
 </script>
 
@@ -20,5 +24,11 @@
     <p>{message}</p>
 {/if}
 {#if !message}
-    <h1>r/{data.forum.name}</h1>
+    <h2>r/{data.forum.name}</h2>
+
+    <ul>
+        {#each posts as post}
+            <li>{post.title}</li>
+        {/each}
+    </ul>
 {/if}
