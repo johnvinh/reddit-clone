16a19a0f1a2aae686b150900ccdc60386675f454
John Vinh <john.michael.vinh2@gmail.com>
2023-04-17 20:46:57 -0400

    Implement get endpoint for retrieving post by id

--- src/main/kotlin/dev/johnvinh/redditclone/controller/PostController.kt
+++ src/main/kotlin/dev/johnvinh/redditclone/controller/PostController.kt
@@ -12,6 +12,7 @@ import io.jsonwebtoken.security.Keys
 import jakarta.servlet.http.HttpServletRequest
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -30,6 +31,11 @@ class PostController(
 
     @GetMapping("")
     fun getPosts() = postService.getAllPosts()
+
+    @GetMapping("/{id}")
+    fun getPostById(@PathVariable id: Long): Post? {
+        return postService.getPostById(id)
+    }
 
     @PostMapping("")
     fun createPost(@RequestBody postRequest: PostRequest, request: HttpServletRequest): ResponseEntity<*> {
--- src/main/kotlin/dev/johnvinh/redditclone/service/PostService.kt
+++ src/main/kotlin/dev/johnvinh/redditclone/service/PostService.kt
@@ -3,12 +3,16 @@ package dev.johnvinh.redditclone.service
 import dev.johnvinh.redditclone.entity.Post
 import dev.johnvinh.redditclone.repository.PostRepository
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 
 @Service
 class PostService @Autowired constructor(private val repository: PostRepository) {
 
     fun getAllPosts(): Iterable<Post> = repository.findAll()
+
+    fun getPostById(id: Long): Post? = repository.findByIdOrNull(id)
+
     fun createPost(post: Post) {
         repository.save(post)
     }
