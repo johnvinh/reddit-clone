1170773871ecc12f3fc0a5290904583716330b36
John Vinh <john.michael.vinh2@gmail.com>
2023-04-21 22:23:03 -0400

    Add link to create new forum to homepage

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -2,12 +2,18 @@
     import {onMount} from "svelte";
     let posts = [];
 
+    let token = "";
+
     onMount(async () => {
         const res = await fetch("/api/post");
         posts = await res.json();
+        token = localStorage.getItem("token");
     });
 </script>
 
+{#if token}
+    <a href="/forums/create">Create Forum</a>
+{/if}
 <ul>
     {#each posts as post}
         <li>{post["title"]}</li>
