644d327f1a6bb8b5a68575ef8837e4bf0004893c
John Vinh <john.michael.vinh2@gmail.com>
2023-05-16 11:13:15 -0400

    Optimize imports

--- src/main/kotlin/dev/johnvinh/redditclone/controller/PostController.kt
+++ src/main/kotlin/dev/johnvinh/redditclone/controller/PostController.kt
@@ -7,17 +7,9 @@ import dev.johnvinh.redditclone.service.PostService
 import dev.johnvinh.redditclone.service.UserService
 import io.jsonwebtoken.Claims
 import io.jsonwebtoken.Jwts
-import io.jsonwebtoken.SignatureAlgorithm
-import io.jsonwebtoken.security.Keys
 import jakarta.servlet.http.HttpServletRequest
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 class PostRequest(var title: String, var textualContent: String, var link: String, var forum: String)
 
