a2478bdadc4354d7c8406c6d1f513cb976af503b
John Vinh <john.michael.vinh2@gmail.com>
2023-05-26 22:15:22 -0400

    Add comment posting authentication

--- src/main/kotlin/dev/johnvinh/redditclone/controller/CommentController.kt
+++ src/main/kotlin/dev/johnvinh/redditclone/controller/CommentController.kt
@@ -1,7 +1,13 @@
 package dev.johnvinh.redditclone.controller
 
+import dev.johnvinh.redditclone.entity.User
+import dev.johnvinh.redditclone.getUserFromJwt
 import dev.johnvinh.redditclone.repository.CommentRepository
+import dev.johnvinh.redditclone.service.CommentService
+import dev.johnvinh.redditclone.service.UserService
 import io.jsonwebtoken.Jwts
+import jakarta.servlet.http.HttpServletRequest
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -11,10 +17,16 @@ class CommentRequest(var content: String)
 
 @RestController
 @RequestMapping("/api/comment")
-class CommentController(private val repository: CommentRepository) {
+class CommentController(private val commentService: CommentService, private val userService: UserService) {
     private val parser = Jwts.parserBuilder().setSigningKey(JwtKey.secretKey).build()
     @PostMapping("/create")
-    fun createComment(@RequestBody commentRequest: CommentRequest) {
+    fun createComment(@RequestBody commentRequest: CommentRequest, request: HttpServletRequest): ResponseEntity<*> {
+        val userResponse = getUserFromJwt(parser, request, userService)
+        if (!userResponse.statusCode.is2xxSuccessful) {
+            return userResponse
+        }
 
+        val user = userResponse.body as User
+        return userResponse
     }
 }
