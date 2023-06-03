a7f87ee91c39224ea95bfa33eb372831e1046a28
John Vinh <john.michael.vinh2@gmail.com>
2023-06-02 22:26:14 -0400

    Document forum controller

--- src/main/kotlin/dev/johnvinh/redditclone/controller/ForumController.kt
+++ src/main/kotlin/dev/johnvinh/redditclone/controller/ForumController.kt
@@ -19,16 +19,39 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RestController
 
+/**
+ * Represents the data received in a request to create a forum.
+ *
+ * @property name The name of the forum.
+ * @property description The description of the forum.
+ */
 class ForumRequest(var name: String, var description: String)
 
+/**
+ * Handles requests related to forums.
+ *
+ * @property userService Provides methods related to users.
+ * @property forumService Provides methods related to forums.
+ */
 @RestController
 @RequestMapping("/api/forum")
 class ForumController(private val userService: UserService, private val forumService: ForumService) {
     private val parser = Jwts.parserBuilder().setSigningKey(JwtKey.secretKey).build()
 
+    /**
+     * Gets a list of all forums.
+     *
+     * @return A list of all forums.
+     */
     @GetMapping("")
     fun getAllForums() = forumService.getAllForums()
 
+    /**
+     * Gets a specific forum by its name. The case of the forum name does not matter.
+     *
+     * @param forum The name of the forum.
+     * @return The forum, or null if no forum with the given name exists.
+     */
     @GetMapping("/{forum}")
     @ResponseBody
     fun getSpecificForum(@PathVariable("forum") forum: String): Forum? {
@@ -36,6 +59,14 @@ class ForumController(private val userService: UserService, private val forumSer
         return forumService.getForumByName(forum)
     }
 
+    /**
+     * Creates a new forum. The request should include a JWT token for user authentication.
+     * If a forum with the requested name already exists, the method will return a bad request.
+     *
+     * @param forumRequest The request body, which includes the name and description of the forum.
+     * @param request The HTTP request, from which the JWT token is extracted.
+     * @return A ResponseEntity that includes a message and the status of the request.
+     */
     @PostMapping("/create")
     fun createForum(@RequestBody forumRequest: ForumRequest, request: HttpServletRequest): ResponseEntity<*> {
         val userResponse = getUserFromJwt(parser, request, userService)
