d180e5de211cf251c653095df4194e713bc6cde3
John Vinh <john.michael.vinh2@gmail.com>
2023-06-02 22:26:44 -0400

    Optimize imports in forum controller

--- src/main/kotlin/dev/johnvinh/redditclone/controller/ForumController.kt
+++ src/main/kotlin/dev/johnvinh/redditclone/controller/ForumController.kt
@@ -1,23 +1,15 @@
 package dev.johnvinh.redditclone.controller
 
+import JwtKey
 import dev.johnvinh.redditclone.entity.Forum
 import dev.johnvinh.redditclone.entity.User
 import dev.johnvinh.redditclone.getUserFromJwt
-import dev.johnvinh.redditclone.repository.ForumRepository
 import dev.johnvinh.redditclone.service.ForumService
 import dev.johnvinh.redditclone.service.UserService
-import dev.johnvinh.redditclone.verifyJwtToken
-import io.jsonwebtoken.Claims
 import io.jsonwebtoken.Jwts
 import jakarta.servlet.http.HttpServletRequest
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 /**
  * Represents the data received in a request to create a forum.
