c03508a830a3f92c31fcda0477095c3ba8cc2dff
John Vinh <john.michael.vinh2@gmail.com>
2023-06-13 21:52:54 -0400

    Add new comment to post's comments property

--- src/main/kotlin/dev/johnvinh/redditclone/controller/CommentController.kt
+++ src/main/kotlin/dev/johnvinh/redditclone/controller/CommentController.kt
@@ -4,6 +4,7 @@ import dev.johnvinh.redditclone.entity.Comment
 import dev.johnvinh.redditclone.entity.User
 import dev.johnvinh.redditclone.getUserFromJwt
 import dev.johnvinh.redditclone.service.CommentService
+import dev.johnvinh.redditclone.service.PostService
 import dev.johnvinh.redditclone.service.UserService
 import io.jsonwebtoken.Jwts
 import jakarta.servlet.http.HttpServletRequest
@@ -17,7 +18,7 @@ class CommentRequest(var content: String, var postId: Long)
 
 @RestController
 @RequestMapping("/api/comment")
-class CommentController(private val commentService: CommentService, private val userService: UserService) {
+class CommentController(private val commentService: CommentService, private val userService: UserService, private val postService: PostService) {
     private val parser = Jwts.parserBuilder().setSigningKey(JwtKey.secretKey).build()
     @PostMapping("/create")
     fun createComment(@RequestBody commentRequest: CommentRequest, request: HttpServletRequest): ResponseEntity<*> {
@@ -28,6 +29,9 @@ class CommentController(private val commentService: CommentService, private val 
 
         val user = userResponse.body as User
         val comment = Comment(user, commentRequest.content)
+        val post = postService.getPostById(commentRequest.postId) ?: return ResponseEntity.badRequest().body("Post not found")
+        post.comments = post.comments.plus(comment)
+        postService.createPost(post)
         return ResponseEntity.ok(commentService.createComment(comment))
     }
 }
