ca8d519f6d9ba3cebfbc953d4bc4dd632a38a504
John Vinh <john.michael.vinh2@gmail.com>
2023-06-13 21:50:24 -0400

    Add post ID to comment request

--- src/main/kotlin/dev/johnvinh/redditclone/controller/CommentController.kt
+++ src/main/kotlin/dev/johnvinh/redditclone/controller/CommentController.kt
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-class CommentRequest(var content: String)
+class CommentRequest(var content: String, var postId: Long)
 
 @RestController
 @RequestMapping("/api/comment")
