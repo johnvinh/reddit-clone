6e6418df7291a3074f8f5eb873719463cdf0f10a
John Vinh <john.michael.vinh2@gmail.com>
2023-06-16 23:07:08 -0400

    Add Kotlin docs to post service

--- src/main/kotlin/dev/johnvinh/redditclone/service/PostService.kt
+++ src/main/kotlin/dev/johnvinh/redditclone/service/PostService.kt
@@ -6,13 +6,38 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 
+/**
+ * Service class for managing posts.
+ *
+ * @property repository Autowired instance of the [PostRepository].
+ * @constructor creates a new instance of [PostService].
+ *
+ * @param repository the repository to manage posts.
+ */
 @Service
 class PostService @Autowired constructor(private val repository: PostRepository) {
 
+    /**
+     * Fetches all posts from the database.
+     *
+     * @return an Iterable of [Post] objects.
+     */
     fun getAllPosts(): Iterable<Post> = repository.findAll()
 
+    /**
+     * Fetches a single post by its ID.
+     *
+     * @param id the ID of the post.
+     * @return the post with the specified ID, or null if no post with such ID exists.
+     */
     fun getPostById(id: Long): Post? = repository.findByIdOrNull(id)
 
+    /**
+     * Creates a new post.
+     *
+     * @param post the post to create.
+     * @return the created post.
+     */
     fun createPost(post: Post): Post {
         return repository.save(post)
     }
