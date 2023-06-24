f9848f1142dae2ea01891f4b6d4349fffde02f3c
John Vinh <john.michael.vinh2@gmail.com>
2023-06-23 23:54:53 -0400

    Add Kotlin docs to comment service

--- src/main/kotlin/dev/johnvinh/redditclone/service/CommentService.kt
+++ src/main/kotlin/dev/johnvinh/redditclone/service/CommentService.kt
@@ -5,8 +5,21 @@ import dev.johnvinh.redditclone.repository.CommentRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
+/**
+ * Service class for managing comments.
+ *
+ * @property repository Autowired instance of the [CommentRepository].
+ * @constructor Creates a new instance of [CommentService].
+ *
+ * @param repository The repository to manage comments.
+ */
 @Service
 class CommentService @Autowired constructor(private val repository: CommentRepository) {
+    /**
+     * Creates a new comment.
+     *
+     * @param comment The comment to be created.
+     */
     fun createComment(comment: Comment) {
         repository.save(comment)
     }
